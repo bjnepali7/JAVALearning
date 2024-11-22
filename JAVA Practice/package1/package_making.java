298e3ca356ff68d2c31d505e5938dc48bdf0c30c
bjnepali7 <109470940+bjnepali7@users.noreply.github.com>
2024-11-22 20:21:35 +0545

    Add files via upload

---
+++ JAVA Practice/package1/package_making.java
@@ -0,0 +1,24 @@
+package package1;
+// package can be made in java which is basically means  folder
+// in command prompt is we want to create the package  javac -d .classname.java
+// to excecute  java package.classname
+public class package_making{
+    private int x;
+    private  int y;
+    private int s;
+    public void setSum(int p,int r){
+        x=p;
+        y=r;
+     s=p+r;
+       
+    }
+    public void Display(){
+        
+        System.out.println("the sum is"+s);
+    }
+
+
+
+}
+    
+
---
+++ JAVA Practice/package2/practice.java
@@ -0,0 +1,12 @@
+package package2;
+import package1.*;
+
+public class practice {
+    public static void main(String[] args) { 
+        
+        package_making n=new package_making();
+        n.setSum(50, 60);
+        n.Display();
+    }
+    
+}
